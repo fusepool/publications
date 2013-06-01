a276c084e262fcb4b1d9d2a43365e5e5a657c9c4
Luigi Selmi <selmi_luigi@hotmail.com>
2013-06-01 16:50:00 +0200

    added test for method that adds a plain text part to the content item

--- src/test/java/eu/fusepool/platform/enhancer/engine/pubmed/PubMedEnhancementEngineTest.java
+++ src/test/java/eu/fusepool/platform/enhancer/engine/pubmed/PubMedEnhancementEngineTest.java
@@ -11,27 +11,39 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.util.Collections;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
 import org.apache.clerezza.rdf.core.MGraph;
 import org.apache.clerezza.rdf.core.Triple;
 import org.apache.clerezza.rdf.core.UriRef;
+import org.apache.clerezza.rdf.core.impl.TripleImpl;
 import org.apache.clerezza.rdf.core.serializedform.Parser;
+import org.apache.clerezza.rdf.ontologies.DCTERMS;
 import org.apache.clerezza.rdf.ontologies.FOAF;
 import org.apache.clerezza.rdf.ontologies.RDF;
 import org.apache.commons.io.IOUtils;
+import org.apache.stanbol.enhancer.contentitem.inmemory.InMemoryBlob;
 import org.apache.stanbol.enhancer.contentitem.inmemory.InMemoryContentItemFactory;
+import org.apache.stanbol.enhancer.servicesapi.Blob;
 import org.apache.stanbol.enhancer.servicesapi.ContentItem;
 import org.apache.stanbol.enhancer.servicesapi.ContentItemFactory;
+import org.apache.stanbol.enhancer.servicesapi.ContentSource;
 import org.apache.stanbol.enhancer.servicesapi.EngineException;
 import org.apache.stanbol.enhancer.servicesapi.EnhancementEngine;
+import org.apache.stanbol.enhancer.servicesapi.helper.ContentItemHelper;
+import org.apache.stanbol.enhancer.servicesapi.impl.ByteArraySource;
 import org.apache.stanbol.enhancer.servicesapi.impl.StringSource;
+import org.apache.stanbol.enhancer.servicesapi.rdf.TechnicalClasses;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import eu.fusepool.platform.enhancer.engine.pubmed.OntologiesTerms;
 import eu.fusepool.platform.enhancer.engine.pubmed.PubMedEnhancementEngine;
 import eu.fusepool.platform.enhancer.engine.pubmed.testutil.MockComponentContext;
 
@@ -89,7 +101,7 @@ public class PubMedEnhancementEngineTest {
 	/*
 	 * Test if subjects of type person are found after the transformation and prints them
 	 */
-	
+	/*
 	@Test
 	public void testTransformXML() {
 		
@@ -104,15 +116,14 @@ public class PubMedEnhancementEngineTest {
 			System.out.println("Error while transforming the XML file into RDF");
 		}
 		
-		int personsNumber = 0;
+		
 		
 		if (! graph.isEmpty()) {
 			
 			// Filter triples for persons
+			Iterator<Triple> ipersons = graph.filter(null, RDF.type, FOAF.Person) ;
 			
-			//Iterator<Triple> ipersons = graph.filter(null, RDF.type, FOAF.Person) ;
-			Iterator<Triple> ipersons = graph.filter(null, RDF.type, null) ;
-			
+			int personsNumber = 0;
 			while (ipersons.hasNext()){
 				personsNumber += 1;
 				Triple triple = ipersons.next();
@@ -129,6 +140,89 @@ public class PubMedEnhancementEngineTest {
 		}
 		
 		//assertTrue("Subjects of type foaf:Person found in the document " + personsNumber, personsNumber == PERSONS_NUMBER);
+		
+	}
+	*/
+	
+	/*
+	 * Test if entity-reference annotations have been added for each entity found after the transformation of the input XML file into RDF.
+	 */
+	/*
+	@Test
+	public void testAddEnhancements() {
+		
+		MGraph xml2rdf = null;
+		
+		try {
+			
+			xml2rdf = engine.transformXML(ci);
+			
+		} catch (EngineException e) {
+			 
+			System.out.println("Error while transforming the XML file into RDF");
+		}
+		
+		MGraph graph = engine.addEnhancements(ci, xml2rdf);
+		
+		int entityReferences = 0;
+		
+		if (! graph.isEmpty()) {
+			
+			// Filter triples for entities annotations
+			Iterator<Triple> ireferences = graph.filter(null, TechnicalClasses.ENHANCER_ENHANCEMENT, null);
+			while (ireferences.hasNext()) {
+				entityReferences += 1;
+				Triple triple = ireferences.next();
+				String enhancement = triple.getSubject().toString();
+				String entity = triple.getObject().toString();
+				System.out.println("Entity references " + entityReferences + ") "  + enhancement + " entity reference: " + entity);
+			}
+			
+		}
+		else {
+			System.out.println("Enhancement graph empty !");
+		}
+		
+		//assertTrue("Entities found in the document " + entityReferences, entityReferences == ENTITIES_REFERENCED);
+	
+	
+	}
+	*/
+	
+	/*
+	 * Test whether a plain text representation part of document has been added to the content item. 
+	 */
+	@Test
+	public void testAddPartToContentItem() {
+		
+		String plainTextPart = "";
+		
+		engine.addPartToContentItem(ci);
+		
+		// Reading part with index 1 of the content item that contain an object of type ContentSource
+		ByteArraySource source = ci.getPart(1, ByteArraySource.class);
+		
+		// Get the MIME TYPE of the part
+		String partMimeType = source.getMediaType();
+		
+		
+		try {
+			
+			plainTextPart = IOUtils.toString(source.getStream());
+			
+			//System.out.println(plainTextPart);
+		}
+		catch(IOException e) {
+			System.out.println("Error while reading plain text part");
+		}
+		
+		Iterator<Triple> iparts = ci.getMetadata().filter(ci.getUri(), DCTERMS.hasPart, null);
+		while(iparts.hasNext()) {
+			System.out.println("Content Item parts: " + iparts.next().getObject().toString());
+		}
+		
+		assertTrue("text/plain".equals(partMimeType));
+		
 		
 	}
 
